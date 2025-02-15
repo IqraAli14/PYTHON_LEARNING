122163b29b61084308667b227baa4238f50c9ab4
Iqra Rehmat Ali <146968006+IqraAli14@users.noreply.github.com>
2025-02-15 22:52:02 +0500

    Create hello_wolrd.py

---
+++ hello_wolrd.py
@@ -0,0 +1,52 @@
+# PRINT FUNCTION
+print("hello world")
+print('hello world')
+
+# collection of character inside "double quotes" or inside 'single quotes' is called string.
+# we can use 'single quotes' inside "double quotes"
+# "double quotes" inside 'single quotes' .
+print("hello 'my' world")
+print('hello "my" world')
+
+#WE CANNOT 
+# print("hello "my" world")  #syntax error 
+# print('hello 'my' world')  #syntax error 
+
+#ESCAPE SEQUENCES
+# single quote \' ='
+# double quote \" ="
+# backslash \\
+# new line \n
+# tab \t
+# backspace \b
+#INSTEAD WE CAN
+print("hello \"my\" world")
+print('hello \'my\' world')
+print("hello \\my\\ world")
+print("hello \nmy\n world")
+print("hello \tmy\t world")
+print("hello \bmy\b world")
+print('it\'s a beautiful day!')
+print("Hello \"Python\" World")
+print("C:\\Users\\Amairah\\Documents")
+print("Python\nis\nfun!")
+print("\tname:Amairah\n \tage:21y \n\tprofession:radiographer")
+# pattern
+print("*\n**\n***\n****\n*****")
+print("1\n22\n333\n4444\n55555")
+print("H\nE\nL\nl\nO")
+print("*\n**\n***\n****\n*****\n****\n***\n**\n*")
+print("learning\b\b\b\bbing")
+
+# escape characters as normal text
+# output: \"\'
+print("\\\" \\\'")
+
+print("this is double backslash \\\\")
+print("this is mountain: /\\/\\/\\/\\/\\")
+print("he is \t awesome")
+print("\\\"\t\\n\t\\t\t\\\'")
+# raw strings
+print("this is double backslash \\\\")
+print(r"this is double backslash \\")
+print(R"this is double backslash \\")
