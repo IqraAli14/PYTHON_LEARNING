65397af55bbf7eda7da6853652c74f36bcb11868
Iqra Rehmat Ali <146968006+IqraAli14@users.noreply.github.com>
2025-03-08 11:50:16 +0500

    Create 04_python_fundamentals_practice.py

---
+++ 04_python_fundamentals_practice.py
@@ -0,0 +1,22 @@
+# print(5==5.0)
+# print(8<=8)
+# print(True and False)
+# print(True and True)
+# print(True or False)
+# print(not(True or False))
+x=2
+x//=3 #x=x//3
+# print(x)
+x=4
+x*=2
+# print(x)
+x=5/2
+# print(x)
+# print(type(x))
+x = None
+# print(type(None))
+
+# print(type(3==5))
+# print(type(range(5)))
+# print(isinstance(5,int))
+# print(type(5//2))
